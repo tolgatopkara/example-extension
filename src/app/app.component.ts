882b42b8517896ea8499b685c2a586e4d6793bfc
Tolga <tolga.topkara96@gmail.com>
2022-11-29 13:20:34 +0300

    standalone=true

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone : true,
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template : `
+  <h1 class="">Angular 2</h1> ` ,
+  styles : [` `]
 })
 export class AppComponent {
   title = 'example-extention';
