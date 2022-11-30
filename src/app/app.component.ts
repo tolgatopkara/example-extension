e9211f20ce07a78a064d4430cf70a764c08b85bb
Tolga <tolga.topkara96@gmail.com>
2022-11-30 09:26:22 +0300

    angular material added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   standalone : true,
   selector: 'app-root',
   template : `
-  <h1 class="">Angular 2</h1> ` ,
+  <h1 class="">Angular</h1> ` ,
   styles : [` `]
 })
 export class AppComponent {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    // provideRouter(appRoutes)
-  ]
+    importProvidersFrom(BrowserAnimationsModule)
+]
 });
