ef7f915e6942f85a62336c74d39fce22cedc55ed
Tolga <tolga.topkara96@gmail.com>
2022-12-22 15:53:09 +0300

    fileName looks cool

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,27 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ExportAsService, ExportAsConfig, ExportAsModule } from 'ngx-export-as';
 import { GaugeComponent } from "../gauge/gauge.component";
 
 @Component({
-    standalone: true,
-    selector: 'app-body',
-    template: `
+  standalone: true,
+  selector: 'app-body',
+  template: `
 
     <div id="downloadedItem" class=" w-120 h-68 bg-zinc-900">
-  <app-gauge></app-gauge>
+  <app-gauge  #gauge></app-gauge>
 </div>
 <button (click)="download()" >download</button>
 
 
  `,
-    imports: [GaugeComponent,ExportAsModule]
+  imports: [GaugeComponent, ExportAsModule]
 })
 export class BodyComponent {
+
+  @ViewChild('gauge')
+  gauge!: GaugeComponent;
+
   exportAsConfig: ExportAsConfig = {
     type: 'png', // the type you want to download
     elementIdOrContent: 'downloadedItem', // the id of html/table element
     options: {
-      scale: 1.00000,
+      scale: 1,
 
       // html2canvas options
     }
@@ -31,8 +35,7 @@ export class BodyComponent {
   constructor(private exportAsService: ExportAsService) { }
 
   download() {
-    this.exportAsService.save(this.exportAsConfig, 'myFile').subscribe(() => {
-
+    this.exportAsService.save(this.exportAsConfig, `${this.gauge.gaugeValue} x ${this.gauge.gaugeValue2}`).subscribe(() => {
       // save started
     });
   }
